49ae0dc0cc38352f2a23669b11974acb70e0dfea
gabriellakambey <kambeygebby@gmail.com>
2021-06-25 21:36:50 +0800

    commit

--- app/src/main/java/com/example/weddingvaganza/view/fragment/CategoryFragment.java
+++ app/src/main/java/com/example/weddingvaganza/view/fragment/CategoryFragment.java
@@ -1,6 +1,7 @@
 package com.example.weddingvaganza.view.fragment;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
@@ -26,6 +29,7 @@ import com.example.weddingvaganza.api.WeddingApi;
 import com.example.weddingvaganza.api.WeddingService;
 import com.example.weddingvaganza.model.AddCategoryResponse;
 import com.example.weddingvaganza.model.CategoryModel;
+import com.example.weddingvaganza.view.activity.ScheduleFromCategoryActivity;
 import com.example.weddingvaganza.view.dialog.AddCategoryDialog;
 import com.pixplicity.easyprefs.library.Prefs;
 
@@ -37,11 +41,8 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class CategoryFragment extends Fragment implements AddCategoryDialog.CategoryDialogListener{
+public class CategoryFragment extends Fragment implements AddCategoryDialog.CategoryDialogListener, ListCategoryAdapter.ClickedItem {
 
-    Fragment fragment;
-    FragmentManager fragmentManager;
-    FragmentTransaction fragmentTransaction;
     Dialog dialog;
     ListCategoryAdapter adapter;
     RecyclerView recyclerView;
@@ -52,36 +53,37 @@ public class CategoryFragment extends Fragment implements AddCategoryDialog.Cate
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_category, container, false);
 
+        // open dialog
         dialog = new Dialog(getContext());
         LinearLayout addCategory = view.findViewById(R.id.add_category);
         addCategory.setOnClickListener(v -> {
             openDialog();
         });
 
+        // recyclerview list category
+        recyclerView = view.findViewById(R.id.rv_listCategory1);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
+        adapter = new ListCategoryAdapter(this::ClickedCategory);
+
+        getAllCategory();
+
         return view;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        // show fragment when data exist
+    private void getAllCategory() {
         int currentUserId = Prefs.getInt("user_id", 0);
         WeddingService weddingService = WeddingApi.getRetrofit().create(WeddingService.class);
         Call<List<CategoryModel>> call = weddingService.getCategory(currentUserId);
         call.enqueue(new Callback<List<CategoryModel>>() {
             @Override
             public void onResponse(Call<List<CategoryModel>> call, Response<List<CategoryModel>> response) {
-                List<CategoryModel> categoryModels = response.body();
-                if (categoryModels.size()!=0) {
-                    Toast.makeText(getContext(), "Data available", Toast.LENGTH_SHORT).show();
-                    fragment = new ListCategoryFragment();
-                    fragmentManager = getFragmentManager();
-                    fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.fl_categoryEvent, fragment);
-                    fragmentTransaction.commit();
-                } else {
-                    Toast.makeText(getContext(), "There is no data", Toast.LENGTH_SHORT).show();
+                if (response.isSuccessful()) {
+                    List<CategoryModel> categoryModels = response.body();
+                    adapter.setData(categoryModels);
+                    recyclerView.setAdapter(adapter);
                 }
             }
 
@@ -90,32 +92,13 @@ public class CategoryFragment extends Fragment implements AddCategoryDialog.Cate
 
             }
         });
-
-    }
-
-    // enable toolbar menu in this fragment
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
     }
 
     @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_schedule,menu);
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        int id = item.getItemId();
-
-        if (id == R.id.schedule_print) {
-            Toast.makeText(getActivity(), "Print", Toast.LENGTH_SHORT).show();
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void openDialog() {
@@ -126,9 +109,13 @@ public class CategoryFragment extends Fragment implements AddCategoryDialog.Cate
         addCategoryDialog.show(getFragmentManager(), "add category");
     }
 
-
     @Override
     public void onAddCategory() {
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void ClickedCategory(CategoryModel categoryModel) {
+        startActivity(new Intent(getActivity(), ScheduleFromCategoryActivity.class).putExtra("data", categoryModel));
     }
 }
