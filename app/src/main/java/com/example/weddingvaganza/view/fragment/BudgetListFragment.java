7dcfca3bbc296eb688a1e926427e2c66bd2fe3ab
dansiapa <48487205+dansiapa@users.noreply.github.com>
2021-08-08 11:54:10 +0800

    front-end budget + payment + profile

--- app/src/main/java/com/example/weddingvaganza/view/fragment/BudgetListFragment.java
+++ app/src/main/java/com/example/weddingvaganza/view/fragment/BudgetListFragment.java
@@ -1,5 +1,6 @@
 package com.example.weddingvaganza.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -10,10 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.weddingvaganza.R;
 import com.example.weddingvaganza.adapter.BudgetRecyclerViewAdapter;
 import com.example.weddingvaganza.model.BudgetModel;
+import com.example.weddingvaganza.view.activity.AddBudgetActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +27,7 @@ public class BudgetListFragment extends Fragment {
     View v;
     private RecyclerView rv_budgetList;
     private List<BudgetModel> budgetModels;
+    Button btn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,6 +37,12 @@ public class BudgetListFragment extends Fragment {
         BudgetRecyclerViewAdapter recyclerViewAdapter = new BudgetRecyclerViewAdapter(getContext(),budgetModels);
         rv_budgetList.setLayoutManager(new LinearLayoutManager(getActivity()));
         rv_budgetList.setAdapter(recyclerViewAdapter);
+
+        btn = v.findViewById(R.id.btn_addBudget);
+        btn.setOnClickListener(v1 -> {
+            Intent n = new Intent(getActivity(), AddBudgetActivity.class);
+            startActivity(n);
+        });
 
         return v;
     }
